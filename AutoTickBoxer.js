d2d9b4feaba4eec77ce3eb9c0661c7a1931f0d5b
Karik Isichei <karik.isichei@gmail.com>
2017-09-07 13:28:31 +0100

    basic functionality working

--- AutoTickBoxer.js
+++ AutoTickBoxer.js
@@ -1,25 +1,40 @@
 // Autotickbox constructor
-function AutoTickBoxer(excludeColumns, divId, dataFilePath){
+function AutoTickBoxer(excludeColumns, divId, data){
 	this.excludeColumns = excludeColumns;
 	this.divId = divId;
-	this.dataFilePath = dataFilePath;
-
-	this.initialise = function(excludeColumns, divId, dataFilePath){
-		d3.csv(this.dataFilePath, function(error, data) {
-
-		var colnames = Object.keys(data[0])
-		var toFilter = colnames.filter(function(d){
-	  		return this.excludeColumns.indexOf(d) == -1;
-	  	})
-
-		for (i = 0; i<toFilter.length; i++){
-			unique_vars = d3.keys(d3.nest().key(function (d) { return d[toFilter[i]]; }).map(data))
-			checkboxes = d3.select(this.divId).append("div").classed("control", true).classed(unique_vars[i] + "_controls", true).selectAll("label").data(unique_vars).enter().append("label").property("value", function(d){return d;});
-
+	this.data = data;
+	this.toFilter = Object.keys(this.data[0]).filter(function(d){return this.excludeColumns.indexOf(d) == -1;});
+	
+	this.generateTickboxes = function(){
+		var col;
+		for (i = 0; i<this.toFilter.length; i++){
+			col = this.toFilter[i];
+			unique_vars = d3.keys(d3.nest().key(function (d) { return d[col];}).map(this.data));
+			checkboxes = d3.select(this.divId).append("div").attr("id",col+"_tickbox_div").classed("control", true).selectAll("label").data(unique_vars).enter().append("label").property("value", function(d){return d;});
 			checkboxes.append("input").property("type","checkbox").property("checked", true);
-
 			checkboxes.append("text").text(function(d){return d;});
 		}
-	  })
+	}
+
+	this.filterData = function(){
+		
+		// Initialise
+		var filteredData = this.data;
+		var tmpArray; 
+
+		// Filter for each set of checkboxesx
+		for(i = 0; i < this.toFilter.length; i++){
+			col = this.toFilter[i];
+			tmpArray = [];
+			// Get the list of tickboxes that are checked
+			d3.select("#"+ col +"_tickbox_div").selectAll("label").selectAll("input").each(function(d){if(this.checked){tmpArray.push(d)}})
+
+			// filter data
+			filteredData = filteredData.filter(function(d){
+	    	return tmpArray.indexOf(d[col]) >= 0;
+	    });
+		}
+
+		return filteredData;
 	}
 }
