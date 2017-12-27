4b6c632a7ca7be9a8299bb8bb6c9625a4ca25475
Karik Isichei <karik.isichei@gmail.com>
2017-12-27 21:48:32 +0000

    Added value rename, select all and column title functionality to autotickboxer. Index has been updated to demonstrate each of the new function for the offence_type column in sankey data.

--- javascript/AutoTickBoxer.js
+++ javascript/AutoTickBoxer.js
@@ -10,39 +10,82 @@ function AutoTickBoxer(excludeColumns, divId, data){
 
 		var col;
 
+		// if returnValue is true function returns obj[key] (if key in obj is defined, otherwise will return null)
+		// if returnValue is false then returns true or false (for is key in object)
+		// default for return value is false
 		var isUndefined = function(obj, key, returnValueIfUndefined){
-			returnValueIfUndefined = (returnValueIfUndefined === 'undefined') ? true : returnValueIfUndefined
-			return (typeof(obj[key]) === 'undefined' ? returnValueIfUndefined : obj[key])
+			if(returnValueIfUndefined == null) {
+				return (typeof(obj[key]) === 'undefined')
+			} else {
+				return (typeof(obj[key]) === 'undefined' ? returnValueIfUndefined : obj[key])
+			}
 		}
-
-    // var getLabel = function (valueName, renameDictionary) {
-    //     return (isUndefined(renameDictionary[valueName]) ? valueName : renameDictionary[valueName]);
-    // }
 
 		for (i = 0; i < this.toFilter.length; i++){
 			col = this.toFilter[i];
 			colOptions = isUndefined(options, col, {}); 
       columnRenameDictionary = isUndefined(colOptions, 'valueNames', {});
-			console.log(columnRenameDictionary);
 			unique_vars = d3.keys(d3.nest().key(function (d) { return d[col];}).map(this.data));
 
-			checkboxes = d3.select(this.divId)
+			checkboxDiv = d3.select(this.divId)
 										.append("div")
 										.attr("id", col+"_tickbox_div")
-										.classed("control", true)
-										.selectAll("label")
+										.classed("control", true);
+
+			// If there is a descriptor for the column
+			if (!isUndefined(colOptions, "name", null)) {
+			   checkboxDiv.append("text")
+			   						.classed("description_autotickboxer", true)
+			   						// .style("font-style", "italic")
+										.style("font-weight", "bold")
+										.text(colOptions["name"])
+			}
+			if (!isUndefined(colOptions, "selectAll", null)) {
+	      selectAllLabel = checkboxDiv.append("label")
+	      									.classed("select_all_autotickboxer", true)
+				      						.property("value", col+"_select_all")
+
+        selectAllLabel.append("input")
+											.classed("autotickboxCheckbox",true)
+											.attr("id", col + "_select_all_AutoTickBoxer")
+											.property("type","checkbox")
+											.property("checked", true)
+											.on("click", function(){
+												// console.log("something happened")
+												
+												// console.log(column_name)
+												// console.log(this.value)
+												console.log(this.checked)
+												column_name = d3.select(this).attr('id').split('_select_all')[0]
+												console.log(column_name)
+
+											  // select all tick boxes and turn them off
+											  d3.selectAll("." + column_name + "_checkbox_autotickboxer").property("checked", this.checked);
+											});
+										
+				selectAllLabel.append("text")
+											.style("font-style", "italic")
+											.style("font-weight", "bold")
+											.text("Select all");
+			}
+			checkboxes = checkboxDiv.selectAll("label:not(.select_all_autotickboxer)")
 										.data(unique_vars).enter()
 										.append("label")
 										.property("value", function(d){return d;});
 
 			checkboxes.append("input")
-								.classed("autotickboxCheckbox",true)
+								.classed("autotickboxCheckbox", true)
+								.classed(col+"_checkbox_autotickboxer", true)
 								.property("type","checkbox")
 								.property("checked", true);
 
 			checkboxes.append("text").text(function(d){return isUndefined(columnRenameDictionary, d, d);});
 		}
 	}
+
+	// d3.selectAll(".select_all_autotickboxer").selectAll("input").on("change", function(){
+
+	// });
 
 	this.filterData = function(){
 		// Initialise
@@ -54,7 +97,7 @@ function AutoTickBoxer(excludeColumns, divId, data){
 			col = this.toFilter[i];
 			tmpArray = [];
 			// Get the list of tickboxes that are checked
-			d3.select("#"+ col +"_tickbox_div").selectAll("label").selectAll("input").each(function(d){if(this.checked){tmpArray.push(d)}})
+			d3.select("#"+ col +"_tickbox_div").selectAll("label:not(.select_all_autotickboxer)").selectAll("input").each(function(d){if(this.checked){tmpArray.push(d)}})
 
 			// filter data
 			filteredData = filteredData.filter(function(d){
