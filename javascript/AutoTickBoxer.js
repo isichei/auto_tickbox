0c0da5177e7eb9d5f6638dffc32d32335f4250d1
Karik Isichei <karik.isichei@gmail.com>
2017-12-28 13:27:39 +0000

    Added dropdown functionality

--- javascript/AutoTickBoxer.js
+++ javascript/AutoTickBoxer.js
@@ -1,29 +1,32 @@
 // Autotickbox constructor
-function AutoTickBoxer(excludeColumns, divId, data){
+function AutoTickBoxer(excludeColumns, divId, data, options){
 	this.excludeColumns = excludeColumns;
 	this.divId = divId;
 	this.data = data;
+	this.options = options;
 	this.toFilter = Object.keys(this.data[0]).filter(function(d){return this.excludeColumns.indexOf(d) == -1;});
 	this.allCheckboxClass = ".autotickboxCheckbox";
-	
-	this.generateTickboxes = function(options){
+
+	this.isUndefined = function(obj, key, returnValueIfUndefined){
+		if(returnValueIfUndefined == null) {
+			return (typeof(obj[key]) === 'undefined')
+		} else {
+			return (typeof(obj[key]) === 'undefined' ? returnValueIfUndefined : obj[key])
+		}
+	}
+
+	this.generateTickboxes = function(){
 
 		var col;
-
+		var isUndefined = this.isUndefined;
 		// if returnValue is true function returns obj[key] (if key in obj is defined, otherwise will return null)
 		// if returnValue is false then returns true or false (for is key in object)
 		// default for return value is false
-		var isUndefined = function(obj, key, returnValueIfUndefined){
-			if(returnValueIfUndefined == null) {
-				return (typeof(obj[key]) === 'undefined')
-			} else {
-				return (typeof(obj[key]) === 'undefined' ? returnValueIfUndefined : obj[key])
-			}
-		}
 
+		// Add column option types
 		for (i = 0; i < this.toFilter.length; i++){
 			col = this.toFilter[i];
-			colOptions = isUndefined(options, col, {}); 
+			colOptions = isUndefined(this.options, col, {}); 
       columnRenameDictionary = isUndefined(colOptions, 'valueNames', {});
 			unique_vars = d3.keys(d3.nest().key(function (d) { return d[col];}).map(this.data));
 
@@ -34,52 +37,60 @@ function AutoTickBoxer(excludeColumns, divId, data){
 
 			// If there is a descriptor for the column
 			if (!isUndefined(colOptions, "name", null)) {
-			   checkboxDiv.append("text")
+			   checkboxDiv.append("label").append("text")
 			   						.classed("description_autotickboxer", true)
 			   						// .style("font-style", "italic")
 										.style("font-weight", "bold")
 										.text(colOptions["name"])
 			}
-			if (!isUndefined(colOptions, "selectAll", null)) {
-	      selectAllLabel = checkboxDiv.append("label")
-	      									.classed("select_all_autotickboxer", true)
-				      						.property("value", col+"_select_all")
+			// Then add the checkboxes or dropdown box
+			if (isUndefined(colOptions, "type", 'checkbox') == 'checkbox') {
+				if (!isUndefined(colOptions, "selectAll", null)) {
+		      selectAllLabel = checkboxDiv.append("label")
+		      									.classed("select_all_autotickboxer", true)
+					      						.property("value", col+"_select_all")
 
-        selectAllLabel.append("input")
-											.classed("autotickboxCheckbox",true)
-											.attr("id", col + "_select_all_AutoTickBoxer")
-											.property("type","checkbox")
-											.property("checked", true)
-											.on("click", function(){
-												// console.log("something happened")
-												
-												// console.log(column_name)
-												// console.log(this.value)
-												console.log(this.checked)
-												column_name = d3.select(this).attr('id').split('_select_all')[0]
-												console.log(column_name)
+	        selectAllLabel.append("input")
+												.classed("autotickboxCheckbox",true)
+												.attr("id", col + "_select_all_AutoTickBoxer")
+												.property("type","checkbox")
+												.property("checked", true)
+												.on("click", function(){
+													column_name = d3.select(this).attr('id').split('_select_all')[0]
+													
+												  // select all tick boxes and turn them off
+												  d3.selectAll("." + column_name + "_checkbox_autotickboxer").property("checked", this.checked);
+												});
+											
+					selectAllLabel.append("text")
+												.style("font-style", "italic")
+												.style("font-weight", "bold")
+												.text("Select all");
+				}
+				checkboxes = checkboxDiv.selectAll("label:not(.select_all_autotickboxer)")
+											.data(unique_vars).enter()
+											.append("label")
+											.property("value", function(d){return d;});
 
-											  // select all tick boxes and turn them off
-											  d3.selectAll("." + column_name + "_checkbox_autotickboxer").property("checked", this.checked);
-											});
-										
-				selectAllLabel.append("text")
-											.style("font-style", "italic")
-											.style("font-weight", "bold")
-											.text("Select all");
+				checkboxes.append("input")
+									.classed("autotickboxCheckbox", true)
+									.classed(col+"_checkbox_autotickboxer", true)
+									.property("type","checkbox")
+									.property("checked", true);
+
+				checkboxes.append("text").text(function(d){return isUndefined(columnRenameDictionary, d, d);});
+			} else {
+				// Create dropbox
+				var select = checkboxDiv.append('select')
+  					.attr('class','select')
+
+				var options = select
+				  .selectAll('option')
+					.data(unique_vars).enter()
+					.append('option')
+						.text(function (d) { return d; });
 			}
-			checkboxes = checkboxDiv.selectAll("label:not(.select_all_autotickboxer)")
-										.data(unique_vars).enter()
-										.append("label")
-										.property("value", function(d){return d;});
-
-			checkboxes.append("input")
-								.classed("autotickboxCheckbox", true)
-								.classed(col+"_checkbox_autotickboxer", true)
-								.property("type","checkbox")
-								.property("checked", true);
-
-			checkboxes.append("text").text(function(d){return isUndefined(columnRenameDictionary, d, d);});
+			
 		}
 	}
 
@@ -90,14 +101,36 @@ function AutoTickBoxer(excludeColumns, divId, data){
 	this.filterData = function(){
 		// Initialise
 		var filteredData = this.data;
-		var tmpArray; 
+		var isUndefined = this.isUndefined;
+		var tmpArray, checkbox, dropdownSelection; 
 
 		// Filter for each set of checkboxesx
 		for(i = 0; i < this.toFilter.length; i++){
 			col = this.toFilter[i];
 			tmpArray = [];
-			// Get the list of tickboxes that are checked
-			d3.select("#"+ col +"_tickbox_div").selectAll("label:not(.select_all_autotickboxer)").selectAll("input").each(function(d){if(this.checked){tmpArray.push(d)}})
+
+			checkbox = true;
+
+			if (!isUndefined(this.options, col, null)) {
+				if (isUndefined(this.options[col], "type", "checkbox") == "dropdown") {
+					checkbox = false
+				}
+			}
+			if (checkbox) {
+				// Get the list of tickboxes that are checked
+				d3.select("#"+ col +"_tickbox_div")
+				.selectAll("label:not(.select_all_autotickboxer)")
+				.selectAll("input")
+				.each(function(d){if(this.checked){tmpArray.push(d)}})
+
+			} else {
+				dropdownSelection = d3.select("#"+ col +"_tickbox_div").select('select').property('value')
+				d3.select("#"+ col +"_tickbox_div")
+				.select("select")
+				.selectAll("option")
+				.each(function(d){if(this.innerHTML == dropdownSelection){tmpArray.push(d)}})
+			}
+			// Add to the list any options that are selected
 
 			// filter data
 			filteredData = filteredData.filter(function(d){
